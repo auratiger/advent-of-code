ebee5b4e42cd6d5c8e77a2d00c4976bfda155aa2
Georgi Georgiev <auratiger00@gmail.com>
2024-12-03 22:29:44 +0200

    go: day 3 part 2 noregex

--- src/days/day-3/day-3.go
+++ src/days/day-3/day-3.go
@@ -35,13 +35,11 @@ func partOne() {
 
 	var result uint64 = 0
 
-	fmt.Println(str)
-
 	r, _ := regexp.Compile("mul\\((\\d{1,3}),(\\d{1,3})\\)")
 
 	res := r.FindAllStringSubmatch(str, 1000)
 
-	fmt.Println(res)
+	// fmt.Println(res)
 
 	for _, val := range res {
 
@@ -73,9 +71,9 @@ func partTwo() {
 
 	r, _ := regexp.Compile("mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)")
 
-	res := r.FindAllStringSubmatch(str, 2000)
+	res := r.FindAllStringSubmatch(str, 1000)
 
-	fmt.Println(res)
+	// fmt.Println(res)
 
 	isEnabled := true
 
@@ -128,21 +126,95 @@ func partOneNoRegex() {
 		varsSplit := strings.Split(vars[1:], ",")
 
 		if len(varsSplit) != 2 {
-			fmt.Println("split not 2", varsSplit)
+			// fmt.Println("split not 2", varsSplit)
 			continue
 		}
 
 		leftVar, err := strconv.Atoi(varsSplit[0])
 
 		if err != nil {
-			fmt.Println("left not int: ", leftVar)
+			// fmt.Println("left not int: ", leftVar)
 			continue
 		}
 
 		rigthVar, err := strconv.Atoi(varsSplit[1])
 
 		if err != nil {
-			fmt.Println("rigth not int: ", rigthVar)
+			// fmt.Println("rigth not int: ", rigthVar)
+			continue
+		}
+
+		result += leftVar * rigthVar
+
+	}
+
+	fmt.Println("result: ", result)
+
+}
+
+func partTwoNoRegex() {
+
+	wd, _ := os.Getwd()
+	filePath := wd + "/src/days/day-3/input.txt"
+	content, _ := os.ReadFile(filePath)
+	str := string(content)
+
+	split := strings.Split(str, "mul")
+
+	result := 0
+	active := true
+	bufferedActive := true
+
+	for _, val := range split {
+		active = bufferedActive
+
+		if active {
+			hasDont := strings.Contains(val, "don't()")
+
+			if hasDont {
+				bufferedActive = false
+			}
+		} else {
+			hasDo := strings.Contains(val, "do()")
+
+			if hasDo {
+				bufferedActive = true
+			}
+
+		}
+
+		if !active {
+			continue
+		}
+
+		if val[0] != '(' {
+			continue
+		}
+
+		hasClosingBracket := strings.IndexByte(val, ')')
+		if hasClosingBracket == -1 {
+			continue
+		}
+
+		vars := val[1:hasClosingBracket]
+		varsSplit := strings.Split(vars, ",")
+
+		if len(varsSplit) != 2 {
+			// fmt.Println("split not 2", varsSplit)
+			continue
+		}
+
+		leftVar, err := strconv.Atoi(varsSplit[0])
+
+		if err != nil {
+			// fmt.Println("left not int: ", leftVar)
+			continue
+		}
+
+		rigthVar, err := strconv.Atoi(varsSplit[1])
+
+		if err != nil {
+			// fmt.Println("rigth not int: ", rigthVar)
 			continue
 		}
 
@@ -161,7 +233,8 @@ func main() {
 
 	// partOne()
 	// partTwo()
-	partOneNoRegex()
+	// partOneNoRegex()
+	partTwoNoRegex()
 
 	var end = time.Since(start)
 
