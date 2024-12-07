243a9bb869d1bbbac2c6b9c1eeb5521b9a0fb831
Georgi Georgiev <auratiger00@gmail.com>
2024-12-07 10:49:02 +0200

    go: day 6 part 2

--- src/days/day-6/day-6.go
+++ src/days/day-6/day-6.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"slices"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -81,7 +82,201 @@ func partOne() {
 }
 
 func partTwo() {
+	wd, _ := os.Getwd()
+	filePath := wd + "/src/days/day-6/input.txt"
+	file, _ := os.Open(filePath)
+	defer file.Close()
 
+	scanner := bufio.NewScanner(file)
+
+	matrix := [][]string{}
+	guardPosition := []int{}
+	guardDirectionIndex := 0
+	guardDirection := directions[guardDirectionIndex]
+
+	for y := 0; scanner.Scan(); y++ {
+		line := strings.Split(strings.TrimSpace(scanner.Text()), "")
+		// fmt.Println(line)
+
+		found := slices.Index(line, "^")
+		if found != -1 {
+			guardPosition = []int{y, found}
+		}
+		matrix = append(matrix, line)
+	}
+
+	matrixYLength := len(matrix)
+	matrixXLength := len(matrix[0])
+
+	obstaclesFound := 0
+	obstacleTurns := make(map[string][][]int)
+
+	result := 0
+
+	for {
+		newYPos := guardPosition[0] + guardDirection[0]
+		newXPos := guardPosition[1] + guardDirection[1]
+
+		matrix[guardPosition[0]][guardPosition[1]] = "X"
+
+		if newYPos >= matrixYLength || newYPos < 0 || newXPos >= matrixXLength || newXPos < 0 {
+			break
+		}
+
+		if obstaclesFound > 2 {
+			nextGuardDirectionIndex := directions[(guardDirectionIndex+1)%len(directions)]
+			newGuardDirectionString := strconv.Itoa(nextGuardDirectionIndex[0]) + strconv.Itoa(nextGuardDirectionIndex[1])
+
+			items, ok := obstacleTurns[newGuardDirectionString]
+
+			isNextDirectionHorizontal := nextGuardDirectionIndex[1] == -1 || nextGuardDirectionIndex[1] == 1
+
+			// fmt.Println("here", items, ok, isNextDirectionHorizontal, guardPosition)
+
+			if ok {
+				for _, item := range items {
+
+					if isNextDirectionHorizontal {
+
+						if item[0] == guardPosition[0] {
+							fmt.Println("hello", guardPosition[0], guardPosition[1])
+
+							isValid := true
+
+							if isValid {
+								result++
+							}
+						}
+
+					} else {
+						if item[1] == guardPosition[1] {
+							fmt.Println("hello", guardPosition[0], guardPosition[1])
+
+							isValid := true
+
+							if isValid {
+								result++
+							}
+						}
+
+					}
+
+				}
+			}
+		}
+
+		if matrix[newYPos][newXPos] == "#" {
+			// change direction
+			// fmt.Println("coor: ", position[0], position[1], " | dir: ", dir)
+
+			guardDirectionString := strconv.Itoa(guardDirection[0]) + strconv.Itoa(guardDirection[1])
+			obstacleTurns[guardDirectionString] = append(obstacleTurns[guardDirectionString], []int{guardPosition[0], guardPosition[1]})
+
+			obstaclesFound++
+
+			guardDirectionIndex = (guardDirectionIndex + 1) % len(directions)
+			guardDirection = directions[guardDirectionIndex]
+		}
+
+		guardPosition[0] = guardPosition[0] + guardDirection[0]
+		guardPosition[1] = guardPosition[1] + guardDirection[1]
+	}
+
+	// fmt.Println(obstacleTurns)
+	//
+	// for _, val := range matrix {
+	// 	fmt.Println(val)
+	// }
+
+	fmt.Println("Result: ", result)
+
+}
+
+func partTwoTwo() {
+	wd, _ := os.Getwd()
+	filePath := wd + "/src/days/day-6/input.txt"
+	input, _ := os.ReadFile(filePath)
+
+	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
+	grid := make([][]rune, len(lines))
+
+	for i, line := range lines {
+		grid[i] = []rune(line)
+	}
+	output := 0
+
+	guardRow := -1
+	guardCol := -1
+	guardDirection := 0
+
+	for row, _ := range grid {
+		if guardRow >= 0 {
+			break
+		}
+		for col, _ := range grid[row] {
+			if grid[row][col] == '^' {
+				guardRow = row
+				guardCol = col
+				break
+			}
+		}
+	}
+
+	directions := [][2]int{
+		{-1, 0},
+		{0, 1},
+		{1, 0},
+		{0, -1},
+	}
+
+	for row := 0; row < len(grid); row++ {
+		for col := 0; col < len(grid[row]); col++ {
+			if grid[row][col] != '.' {
+				continue
+			}
+
+			grid[row][col] = '#'
+
+			visitedLocations := make(map[[3]int]bool)
+			currentRow := guardRow
+			currentCol := guardCol
+			currentDirection := guardDirection
+
+			loopDetected := false
+
+			for {
+				guardState := [3]int{currentRow, currentCol, currentDirection}
+				if visitedLocations[guardState] {
+					loopDetected = true
+					break
+				}
+
+				visitedLocations[guardState] = true
+
+				nextGuardRow := currentRow + directions[currentDirection][0]
+				nextGuardCol := currentCol + directions[currentDirection][1]
+
+				if nextGuardRow < 0 || nextGuardRow >= len(grid) || nextGuardCol < 0 || nextGuardCol >= len(grid[0]) {
+					break
+				}
+
+				if grid[nextGuardRow][nextGuardCol] == '#' {
+					currentDirection = (currentDirection + 1) % 4
+				} else {
+					currentRow = nextGuardRow
+					currentCol = nextGuardCol
+				}
+			}
+
+			if loopDetected {
+				output++
+			}
+
+			grid[row][col] = '.'
+		}
+	}
+
+	fmt.Println("Output Day 6 Part 2", output)
 }
 
 func main() {
@@ -89,8 +284,9 @@ func main() {
 
 	var start = time.Now()
 
-	partOne()
+	// partOne()
 	// partTwo()
+	partTwoTwo()
 
 	var end = time.Since(start)
 
